4806b11ae9ac055f1ffcda0f1ea9842860dcfd91
greener741 <77332500+greener741@users.noreply.github.com>
2026-04-03 14:58:39 +0200

    Update f2.sql

--- f2.sql
+++ f2.sql
@@ -4,16 +4,16 @@ col file_name format a100
 col TABLESPACE_NAME format a15
 
 break on TABLESPACE_NAME skip 1
-COMPUTE SUM LABEL 'TOTAL' of 'Size MB' on TABLESPACE_NAME
-COMPUTE SUM LABEL 'TOTAL' of 'Free MB' on TABLESPACE_NAME
+COMPUTE SUM LABEL 'TOTAL' of 'Size GB' on TABLESPACE_NAME
+COMPUTE SUM LABEL 'TOTAL' of 'Free GB' on TABLESPACE_NAME
 
 select a.TABLESPACE_NAME,
        a.FILE_NAME,
-       round(a.bytes/1024/1024,2) "Size MB",
-       ceil(nvl(sum(b.bytes)/1024/1024,0)) "Free MB",
-       nvl(round(round(sum(b.bytes)/1024/1024,2)*100/round(a.bytes/1024/1024,2)),0) "Free %",
+       round(a.bytes/1024/1024/1024,2) "Size GB",
+       ceil(nvl(sum(b.bytes)/1024/1024/1024,0)) "Free GB",
+       nvl(round(round(sum(b.bytes)/1024/1024/1024,2)*100/round(a.bytes/1024/1024/1024,2)),0) "Free %",
        a.AUTOEXTENSIBLE,
-       round(a.MAXBYTES/1024/1024) "Max Size MB"
+       round(a.MAXBYTES/1024/1024/1024) "Max Size GB"
 from dba_data_files a, dba_free_space b
 where a.file_id=b.file_id(+)
 and a.tablespace_name like '&1'
